1e3d1e0051ba83d831e45c96d7dfd9bbc5f168bb
Michael Loipfuehrer <michael.loipfuehrer@stusta.de>
2020-03-10 20:51:24 +0100

    add rupprecht

---
+++ rupprecht/main.py
@@ -0,0 +1,22 @@
+import gc
+print("GC preboot", gc.mem_free())
+try:
+    import rupprecht
+
+    import webrepl
+    webrepl.start()
+    gc.collect()
+
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+
+    rupprecht.main()
+except KeyboardInterrupt:
+    pass
+except Exception as e:
+    print("error", e)
+    import sys
+    sys.print_exception(e)
+    import machine
+    machine.reset()
